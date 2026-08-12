5317bafedffab3acafe248e14b402edb774c47ec
TypeNil <145931244+TypeNil@users.noreply.github.com>
2026-08-12 17:55:07 +0300

    Project Foundations: CI/CD, Static Analysis, and Modern UI (#1)
    * Setup project: updated dependencies, added CI, detekt config, README and edge-to-edge support
    
    * fix(ci): grant execution permission for gradlew

---
+++ README.md
@@ -0,0 +1,5 @@
+# HH Jobs
+
+![Android CI](https://github.com/TypeNil/hh-jobs-client/actions/workflows/android.yml/badge.svg)
+
+Android-приложение для удобного поиска и мониторинга вакансий.
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,15 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
 }
